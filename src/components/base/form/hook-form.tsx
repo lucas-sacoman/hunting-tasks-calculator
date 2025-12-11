e7dbe99e7815e0857713f90c157ca08a7278ea49
lucas-sacoman <lucas.sacoman1999@gmail.com>
2025-12-11 15:56:31 -0300

    adicionando react hook form

--- src/components/base/form/hook-form.tsx
+++ src/components/base/form/hook-form.tsx
@@ -5,71 +5,71 @@ import type { Control, FieldPath, FieldValues, UseControllerReturn, UseFormRetur
 import { FormProvider, useController, useFormContext } from "react-hook-form";
 
 interface FormProps<TFieldValues extends FieldValues = FieldValues> extends ComponentPropsWithoutRef<typeof AriaForm> {
-    form: UseFormReturn<TFieldValues>;
-    children: ReactNode;
+  form: UseFormReturn<TFieldValues>;
+  children: ReactNode;
 }
 
 interface FormFieldProps<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>> {
-    name: TName;
-    control: Control<TFieldValues>;
-    children: ReactNode | ((control: UseControllerReturn<TFieldValues, TName>) => ReactNode);
+  name: TName;
+  control: Control<TFieldValues>;
+  children: ReactNode | ((control: UseControllerReturn<TFieldValues, TName>) => ReactNode);
 }
 
 interface FormFieldContextValues<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>> {
-    id: string;
-    name: TName;
-    control?: UseControllerReturn<TFieldValues, TName>;
+  id: string;
+  name: TName;
+  control?: UseControllerReturn<TFieldValues, TName>;
 }
 
 const FormFieldContext = createContext<FormFieldContextValues>({} as FormFieldContextValues);
 
 export const useFormFieldContext = () => {
-    const context = useContext(FormFieldContext);
-    const { getFieldState, formState } = useFormContext();
-    const fieldState = getFieldState(context.name, formState);
+  const context = useContext(FormFieldContext);
+  const { getFieldState, formState } = useFormContext();
+  const fieldState = getFieldState(context.name, formState);
 
-    if (!context) {
-        throw new Error("The 'useFormContext' hook must be used within a '<FormField />'");
-    }
+  if (!context) {
+    throw new Error("The 'useFormContext' hook must be used within a '<FormField />'");
+  }
 
-    return { ...context, ...fieldState };
+  return { ...context, ...fieldState };
 };
 
 export const HookForm = <TFieldValues extends FieldValues = FieldValues>({ form, ...props }: FormProps<TFieldValues>) => {
-    return (
-        <FormProvider {...form}>
-            <AriaForm {...props} />
-        </FormProvider>
-    );
+  return (
+    <FormProvider {...form}>
+      <AriaForm {...props} />
+    </FormProvider>
+  );
 };
 
 HookForm.displayName = "HookForm";
 
 export const FormField = <TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>>({
-    children,
-    ...props
+  children,
+  ...props
 }: FormFieldProps<TFieldValues, TName>) => {
-    const id = "form-item-" + useId();
-    const control = useController(props);
-    const withValidationBehavior = {
-        ...control,
-        field: {
-            ...control.field,
-            validationBehavior: "aria",
-        },
-    };
+  const id = "form-item-" + useId();
+  const control = useController(props);
+  const withValidationBehavior = {
+    ...control,
+    field: {
+      ...control.field,
+      validationBehavior: "aria",
+    },
+  };
 
-    return (
-        <FormFieldContext.Provider
-            value={{
-                id,
-                name: props.name,
-                control: control as UseControllerReturn<FieldValues, TName>,
-            }}
-        >
-            {children && (typeof children === "function" ? children(withValidationBehavior) : children)}
-        </FormFieldContext.Provider>
-    );
+  return (
+    <FormFieldContext.Provider
+      value={{
+        id,
+        name: props.name,
+        control: control as UseControllerReturn<FieldValues, TName>,
+      }}
+    >
+      {children && (typeof children === "function" ? children(withValidationBehavior) : children)}
+    </FormFieldContext.Provider>
+  );
 };
 
 FormField.displayName = "FormField";
